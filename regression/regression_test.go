7e6399d5468121df74c87c97ac3fc3a2efec872b
zanhsieh <zanhsieh@gmail.com>
2017-01-11 23:47:16 +0800

    finish regression_test.go

--- regression/regression_test.go
+++ regression/regression_test.go
@@ -2,6 +2,7 @@
 package regression
 
 import (
+	"math"
 	//"reflect"
 	"testing"
 
@@ -44,5 +45,37 @@ func TestSolve(t *testing.T) {
 	}
 }
 
-//func TestVandermonde(t *testing.T) {}
-//func TestPredict(t *testing.T) {}
+func TestVandermonde(t *testing.T) {
+	var (
+		x      = []float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+		c0     = []float64{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+		c1     = []float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+		c2     = []float64{0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100}
+		degree = 2
+	)
+	a := Vandermonde(x, degree)
+	e := mat64.NewDense(len(x), degree+1, nil)
+	e.SetCol(0, c0)
+	e.SetCol(1, c1)
+	e.SetCol(2, c2)
+	if !mat64.EqualApprox(a, e, 0.5) {
+		t.Errorf("expected \n%v\n got \n%v\n", mat64.Formatted(e), mat64.Formatted(a))
+	}
+}
+
+func TestPredict(t *testing.T) {
+	var (
+		thisP  = 11.0
+		cf     = []float64{1, 2, 3}
+		degree = 2
+	)
+	c := mat64.NewDense(len(cf), 1, cf)
+	result := Predict(thisP, c, degree)
+	expected := 0.0
+	for i := range cf {
+		expected = expected + cf[i]*math.Pow(thisP, float64(i))
+	}
+	if result != expected {
+		t.Errorf("expected \n%v\n got \n%v\n", expected, result)
+	}
+}
